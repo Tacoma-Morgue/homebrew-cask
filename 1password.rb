6c2a08409218f58f24fea23c902944169b75c641
Morgan Dana <65994623+Tacoma-Morgue@users.noreply.github.com>
2021-08-12 21:14:50 -0700

    Add files via upload

---
+++ 1password.rb
@@ -0,0 +1,29 @@
+cask "1password" do
+  version "7.8.7"
+  sha256 "e3f39e32ef65b4bba96fcdbc9e467b13d66df9055c53319feb7cbfe5e2e6e328"
+
+  url "https://c.1password.com/dist/1P/mac#{version.major}/1Password-#{version}.zip"
+  name "1Password"
+  desc "Password manager that keeps all passwords secure behind one password"
+  homepage "https://1password.com/"
+
+  livecheck do
+    url "https://app-updates.agilebits.com/product_history/OPM#{version.major}"
+    strategy :page_match
+    regex(%r{href=.*?/1Password-(\d+(?:\.\d+)*)\.pkg}i)
+  end
+
+  auto_updates true
+  conflicts_with cask: "homebrew/cask-versions/1password-beta"
+  depends_on macos: ">= :high_sierra"
+
+  app "1Password #{version.major}.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/*.agilebits.onepassword*",
+    "~/Library/Containers/*.agilebits.onepassword*",
+    "~/Library/Group Containers/2BUA8C4S2C.com.agilebits",
+    "~/Library/Logs/1Password",
+    "~/Library/Preferences/com.agilebits.onepassword*",
+  ]
+end
